4878b149e885ddc7cbfa832fc607254559cff7d4
eolhing <eolhing@gmail.com>
2014-11-01 20:18:34 -0400

    Initial files

---
+++ hook.js
@@ -0,0 +1,14 @@
+var gith = require('gith').create(9001);
+
+var execFile = require('child_process').execFile;
+
+gith({repo: 'eolhing/website'}).on('all', function(payload)
+{
+    if(payload.branch === 'master')
+    {
+		execFile('hook.sh', function(error, stdout, stderr)
+		{
+				console.log('exec complete');
+		});
+    }
+});
