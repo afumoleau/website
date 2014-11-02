902e4b8715d5f6d78fd1a82d96e4a5317254e6d4
eolhing <eolhing@gmail.com>
2014-11-01 21:52:08 -0400

    fix hook script

--- hook.js
+++ hook.js
@@ -1,22 +1,17 @@
 var gith = require('gith').create(9001);
-var winston = require('winston');
 
 var execFile = require('child_process').execFile;
 
-
-winston.add(winston.transports.File, { filename: 'tmp.log' });
-winston.remove(winston.transports.Console);
-
 gith({repo: 'eolhing/website'}).on('all', function(payload)
 {
-	winston.info("repo");
     if(payload.branch === 'master')
     {
-		winston.info("branch");
-		execFile('hook.sh', function(error, stdout, stderr)
-		{
-				winston.info("exec");
-				console.log('exec complete');
-		});
+  		execFile('./hook.sh', function(error, stdout, stderr)
+  		{
+        console.log('Error : ',error);
+        console.log('stdout : ',stdout);
+        console.log('stderr : ',stderr);
+  			console.log('exec complete');
+  		});
     }
 });
