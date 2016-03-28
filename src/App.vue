bf5508ec3b93110fe46aa16b3c7191b2034cc423
Alix <alix.fumoleau@gmail.com>
2016-03-29 01:27:50 +0200

    Add links and update layout

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
-  <div id="app">
+  <div id="app" class="container">
     <header layout horizontal>
         <img src="assets/picture.png" alt="">
         <div class="info" layout vertical>
           <h1>Alix Fumoleau</h1>
             <p>
-              Hi ! I'm a student in Computer Science at Université de Bordeaux.
-              I love programming and I make video games.
+              Hi! I'm a software developer developer at BeTomorrow, France. I work on 3D environments and virtual reality.<br>
+              On my spare time, I'm creating video games, I'm a absolute fan of game jams!
             </p>
           <div class="links">
             Follow me on
@@ -16,12 +16,18 @@
           </div>
         </div>
     </header>
+
+    <div class="row">
+      <div class="input-field col s11">
+        <i class="material-icons prefix">search</i>
+        <input id="searchFilter"  type="search" v-model="searchFilter">
+        <!-- <label class="active" for="searchFilter">Recherche</label> -->
+      </div>
+    </div>
+
+    <project v-for="project in projects | filterBy searchFilter" :project="project"></project>
   </div>
 
-  <div id="main">
-    <input id="searchFilter" type="search" v-model="searchFilter">
-    <project v-for="project in projects | filterBy searchFilter in'name'" :project="project"></project>
-  </div>
 </template>
 
 <script>
@@ -36,65 +42,65 @@ export default {
       searchFilter: '',
       projects: [
         {id: 'hardcorefarmers', name: 'Hardcore Farmers', description: 'Global Game Jam 2016',
-        tags: ['Unity3D'], sources: 'http://google.fr'},
+        tags: ['Unity3D']},
         {id: 'thedryingseed', name: 'The Drying Seeds', description: 'Game Jam Bordeaux #3',
-        tags: ['Unity3D'], sources: 'http://google.fr'},
+        tags: ['Unity3D']},
         {id: 'quantumarcana', name: 'Quantum Arcana', description: 'A video game playable at the Musée d\'Aquitaine in Bordeaux',
-        tags: ['Unity3D'], sources: 'http://google.fr'},
+        tags: ['Unity3D']},
         {id: 'cerberus', name: 'Cerberus', description: 'Ludum Dare #33',
-        tags: ['Unity3D'], sources: 'http://google.fr'},
+        tags: ['Unity3D']},
         {id: 'tkbv', name: 'The Knight of the Buffet à Vaisselle', description: 'Global Game Jam 2015',
-        tags: ['Unity3D'], sources: 'http://google.fr'},
+        tags: ['Unity3D']},
         {id: 'remoteisland', name: 'Remote Island', description: 'Ludum Dare #31, "Entire Game on One Screen"',
-        tags: ['Haxe', 'OpenFL'], sources: 'http://google.fr'},
+        tags: ['Haxe', 'OpenFL'], play: 'http://www.eolhing.me/remoteisland', sources: 'https://github.com/eolhing/remoteisland'},
         {id: 'absolutezero', name: 'Absolute Zero', description: 'Game Jam Bordeaux #2, Theme : Shiver',
-        tags: ['Unity3D'], sources: 'http://google.fr'},
+        tags: ['Unity3D'], download: 'https://drive.google.com/file/d/0By2YCm_3D0JKVWdVVVVwMWhpN00/view', sources: 'https://github.com/eolhing/AbsoluteZero'},
         {id: 'taleofalostsoul', name: 'Tale of a Lost Soul', description: 'Ludum Dare #27, Theme : 10 Secondes',
-        tags: ['Haxe', 'OpenFL'], sources: 'http://google.fr'},
+        tags: ['Haxe', 'OpenFL'], download: 'http://gamejolt.com/games/adventure/tale-of-a-lost-soul/17225/', sources: 'https://github.com/eolhing/taleofalostsoul'},
         {id: 'rhythmpropaganda', name: 'RhythmPropaGanda', description: 'Game Jam Bordeaux, Theme : Rhythm',
-        tags: ['Haxe', 'OpenGL'], sources: 'http://google.fr'},
+        tags: ['Haxe', 'OpenGL'], play: 'http://gamejambordeaux.flat226.fr/files/RythmPropaGanda', sources: 'https://github.com/eolhing/rhythmpropaganda'},
         {id: 'rool', name: 'Rool', description: 'Created with the Flat226 team',
-        tags: ['Unity3D'], sources: 'http://google.fr'},
+        tags: ['Unity3D'], play: 'http://rool.flat226.fr/web/'},
         {id: 'shards', name: 'Shards', description: 'Ludum Dare #30, Theme : Connected Worlds',
-        tags: ['Haxe', 'Heaps'], sources: 'http://google.fr'},
+        tags: ['Haxe', 'Heaps'], play: 'http://eolhing.me/shards', sources: 'https://github.com/eolhing/shards'},
         {id: 'ihatecircles', name: 'I Hate Circles', description: 'Created with the Flat226 team',
-        tags: ['Haxe', 'Haxor'], sources: 'http://google.fr'},
+        tags: ['Haxe', 'Haxor']},
         {id: 'imblockible', name: 'Imblockible', description: 'A game where you have to jump over obstacles',
-        tags: ['C++', 'SFML'], sources: 'http://google.fr'},
+        tags: ['C++', 'SFML']},
         {id: 'droidshooter', name: 'DroidShooter', description: 'A shoot\'em\'up for Android',
-        tags: ['Java', 'LibGDX'], sources: 'http://google.fr'},
+        tags: ['Java', 'LibGDX'], sources: 'https://github.com/eolhing/droidshooter'},
         {id: 'tictactoe', name: 'Tic Tac Toe', description: 'A TicTacToe game to play with your friends',
-        tags: ['C++', 'SFML'], sources: 'http://google.fr'},
+        tags: ['C++', 'SFML'], sources: 'https://github.com/eolhing/tictactoe'},
         {id: 'vrvizualizer', name: 'VR Visualizer', description: 'Student project for Virtual Reality',
-        tags: ['C++', 'OpenFrameworks'], sources: 'http://google.fr'},
+        tags: ['C++', 'OpenFrameworks']},
         {id: 'raytracer', name: 'RayTracer', description: 'Student project for Image Rendering',
-        tags: ['C++', 'OpenGL'], sources: 'http://google.fr'},
+        tags: ['C++', 'OpenGL']},
         {id: 'mapgenerator', name: 'Map Generator', description: 'Tool for generating 2D biome maps',
-        tags: ['C++', 'SFML'], sources: 'http://google.fr'},
+        tags: ['C++', 'SFML']},
         {id: 'hxiris', name: 'hxIris', description: 'Experiment on Virtual Reality',
-        tags: ['Haxe', 'OpenFrameworks'], sources: 'http://google.fr'},
+        tags: ['Haxe', 'OpenFrameworks'], sources: 'https://github.com/eolhing/hxIris'},
         {id: 'towerrift', name: 'Tower Rift', description: 'Student project with Kinect and Oculus Rift',
-        tags: ['Unity3D'], sources: 'http://google.fr'},
+        tags: ['Unity3D'], sources: 'https://github.com/eolhing/towerRift'},
         {id: 'faceblur', name: 'Faceblur', description: 'Internship project on automatic face blurring',
-        tags: ['C++', 'OpenCV', 'Qt'], sources: 'http://google.fr'},
+        tags: ['C++', 'OpenCV', 'Qt']},
         {id: 'tuliptwitter', name: 'Twitter Plugin for Tulip',
-        tags: ['C++', 'Qt'], sources: 'http://google.fr'},
+        tags: ['C++', 'Qt']},
         {id: 'asfml', name: 'aSFML',
-        tags: ['C++', 'SFML'], sources: 'http://google.fr'},
+        tags: ['C++', 'SFML'], sources: 'https://github.com/eolhing/aSFML'},
         {id: 'automathaxe', name: 'automatHaxe',
-        tags: ['Haxe'], sources: 'http://google.fr'},
+        tags: ['Haxe'], sources: 'https://github.com/eolhing/automathaxe'},
         {id: 'croustibash', name: 'Croustibash',
-        tags: ['C++', 'SFML'], sources: 'http://google.fr'},
+        tags: ['C++', 'SFML']},
         {id: 'gameoflife', name: 'Game of Life',
-        tags: ['C++', 'SFML'], sources: 'http://google.fr'},
+        tags: ['C++', 'SFML']},
         {id: 'treegenerator', name: 'Tree Generator',
-        tags: ['C++', 'SFML'], sources: 'http://google.fr'},
+        tags: ['C++', 'SFML']},
         {id: 'flat226', name: 'Flat226', description: 'Association Bordelaise de création de jeux vidéo',
-        tags: ['HTML', 'CSS', 'Haxe'], sources: 'http://google.fr'},
+        tags: ['HTML', 'CSS', 'Haxe']},
         {id: 'labeli', name: 'Label[i]', description: 'Asso étudiante Informatique de l\'Univ. de Bdx',
-        tags: ['HTML', 'CSS', 'Haxe'], sources: 'http://google.fr'},
+        tags: ['HTML', 'CSS', 'Haxe']},
         {id: 'farore', name: 'Farore', description: 'Framework PHP/JS featuring native AJAX loading',
-        tags: ['PHP', 'Javascript'], sources: 'http://google.fr'}
+        tags: ['PHP', 'Javascript'], sources: 'https://github.com/eolhing/Farore'}
       ]
     }
   }
@@ -118,8 +124,6 @@ a {
 header {
 	display : flex;
 	flex-direction: row;
-	max-width: 800px;
-	margin:auto;
 	padding: 20px 0px;
 	img {
 		flex-grow : 0;
@@ -148,10 +152,10 @@ header {
 		}
 	}
 }
-#main {
+/*#main {
 	max-width: 800px;
 	margin:auto;
-}
+}*/
 h2 {
 	font-size : 2em;
 }
